bb399183b22ff1621cbfc39ed275fd957c2e942b
Gabriel Sanchez <gabrielsz87@gmail.com>
2023-10-19 10:00:11 -0500

    add starter code

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # TODO: Write the code that calls the sentiment analysis functions here.
+    # hint: use request.method == "POST"
+    return render_template('form.html')
+
+if __name__ == "__main__":
+    app.run()
